cb1ccc152a35ebebb9ad57e60733d49bd9d00c75
Khushal Abrol <khushalabrol14@gmail.com>
2022-05-01 17:37:16 +0530

    Added: mat-button

--- src/app/lost-found/lost-found.module.ts
+++ src/app/lost-found/lost-found.module.ts
@@ -6,10 +6,13 @@ import { FoundComponent } from './found/found.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+
 import { FeedModule } from './feed/feed.module';
 
 import { FormsModule } from '@angular/forms';
 import { LostFoundComponent } from './lost-found.component';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { LostFoundComponent } from './lost-found.component';
     CommonModule,
     MatTabsModule,
     MatGridListModule,
+    MatButtonModule,
     FeedModule,
     FormsModule
   ], exports: [
