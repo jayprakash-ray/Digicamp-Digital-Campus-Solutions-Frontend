ced7f76b4989a7297e20e35c286f31c226bd1916
Jayprakash Ray <r.jay3283@gmail.com>
2022-05-10 19:50:05 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCpRzDbmNoRM7bYpewJ-uhoOYN_9F26q1o",
+    authDomain: "digicamp-backend.firebaseapp.com",
+    databaseURL: "https://digicamp-backend-default-rtdb.firebaseio.com",
+    projectId: "digicamp-backend",
+    storageBucket: "digicamp-backend.appspot.com",
+    messagingSenderId: "140781661243",
+    appId: "1:140781661243:web:a4c05435d9d2e661b3001e",
+    measurementId: "G-CCPHR6GV5W"
+  }
 };
+
+export const baseUrl = `http://172.16.131.123:9090`;
