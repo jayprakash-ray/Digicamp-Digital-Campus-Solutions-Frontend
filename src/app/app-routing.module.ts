e6aa58b88ba719865d81d21e7e456ecfccbf5510
Khushal Abrol <khushalabrol14@gmail.com>
2022-05-10 14:35:30 +0530

    Added: About and contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { CouriersComponent } from './courier-handling/couriers/couriers.componen
 import { DefaultComponent } from './layouts/default/default.component';
 import { LoginComponent } from './login/login.component';
 import { LostFoundComponent } from './lost-found/lost-found.component';
+import { AboutComponent } from './shared/components/about/about.component';
+import { ContactsComponent } from './shared/components/contacts/contacts.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthGuard } from './_auth/auth.guard';
 
@@ -19,7 +21,9 @@ const routes: Routes = [
       { path: 'package', component: CouriersComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } },
       { path: 'lost-found', component: LostFoundComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } },
       { path: 'add-courier', component: AddCourierComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } },
-      { path: 'history', component: CourierHistoryComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } }
+      { path: 'history', component: CourierHistoryComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } },
+      { path: 'about', component: AboutComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } },
+      { path: 'contacts', component: ContactsComponent, canActivate: [AuthGuard], data: { role: ['student', 'admin'] } }
     ]
   }
 ];
